f16b67e46dcb17581d95df9f7787b84ab0081bca
bjm37 <bjm37@calvin.edu>
2017-09-19 20:45:32 -0400

    Finished basic lab 5

--- labFive/Program.cs
+++ labFive/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace labFive
 {
@@ -10,6 +6,74 @@ namespace labFive
     {
         static void Main(string[] args)
         {
+            bool run = true;
+            while (run == true)
+            {
+                int min, max, i = 0;
+                Random rando = new Random();
+                Console.WriteLine("Welcome to the Grand Circus Casino!");
+                run = RollDie();
+                Console.WriteLine("Cool! How many sides should each die have?");
+                int input = int.Parse(Console.ReadLine());
+                min = 1;
+                max = ++input;
+                Console.WriteLine("Roll 1:");
+
+                while (i < 2)
+                {
+                    Console.WriteLine(rando.Next(min, max));
+                    i++;
+                }
+                run = Continue();
+            }
+
+
+        }
+
+        public static bool RollDie()
+        {
+            Console.WriteLine("Roll the die? y/n");
+            string answer1 = Console.ReadLine();
+            answer1 = answer1.ToLower();
+            bool next;
+            if (answer1 == "y")
+            {
+                next = true;
+            }
+            else if (answer1 == "n")
+            {
+                next = false;
+            }
+            else
+            {
+                Console.WriteLine("I don't understand that, let's try again");
+                next = RollDie();
+            }
+
+            return next;
+        }
+
+        public static bool Continue()
+        {
+            Console.WriteLine("Do you wish to Continue? y/n");
+            string input = Console.ReadLine();
+            input = input.ToLower();
+            bool goOn;
+            if (input == "y")
+            {
+                goOn = true;
+            }
+            else if (input == "n")
+            {
+                goOn = false;
+            }
+            else
+            {
+                Console.WriteLine("I don't understand that, let's try again");
+                goOn = Continue();
+            }
+
+            return goOn;
         }
     }
 }
